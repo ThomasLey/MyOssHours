6beed9a06bd3016578cf41123d318fbe8545e70e
Thomas Ley <thomas-ley@outlook.de>
2024-02-03 18:46:39 +0100

    Quickstart 1 of Duende Identity Server

---
+++ src/MyOssHours/MyOssHours/Program.cs
@@ -0,0 +1,54 @@
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MyOssHoursScope.Read", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "myosshours.read");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast =  Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//});
+
+app.MapGet("/identity", (ClaimsPrincipal user) => 
+        user.Claims.Select(c => new { c.Type, c.Value }))
+    .RequireAuthorization("MyOssHoursScope.Read");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ src/MyOssIdentity/MyOssIdentityServer/Config.cs
@@ -0,0 +1,38 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace MyOssIdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new ApiScope(name: "myosshours.read", displayName: "My OSS Hours Read Access"),
+                new ApiScope(name: "myosshours.contribute", displayName: "My OSS Hours Contribute Access")
+            };
+
+public static IEnumerable<Client> Clients =>
+        new Client[]
+            { new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "myosshours.read", "myosshours.contribute" }
+            }};
+}
